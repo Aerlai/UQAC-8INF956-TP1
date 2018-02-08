d0e77c49e1167dea43a512b4911bc0f2c3bceed0
Antoine GALL (Aerlai) <antoine.gall1@uqac.ca>
2018-02-08 10:35:48 -0500

    blop

--- src/devoir2/question2/Banque.java
+++ src/devoir2/question2/Banque.java
@@ -1,4 +1,18 @@
 package devoir2.question2;
 
 public class Banque {
+    // Variable
+    private int nombreDeClient = 0;
+    private Client tabClient[];
+
+    // Constructeur
+    public Banque(){
+
+    }
+
+    // Methodes
+    public void ajouterClient(String nom){
+        tabClient[nombreDeClient] = new Client(nom);
+        nombreDeClient++;
+    }
 }
--- src/devoir2/question2/Client.java
+++ src/devoir2/question2/Client.java
@@ -1,4 +1,38 @@
 package devoir2.question2;
 
 public class Client {
+    // Variable
+    private Compte tabCompte[];
+    private String nom;
+    private int numeroDeClient;
+    private int nombreDeComptes;
+
+    // Constructeur
+    public Client(String nom, int id){
+        this.nom = nom;
+        this.numeroDeClient = id;
+        this.nombreDeComptes = 0;
+    }
+
+    // Getters
+    public int getNbreCompte(){
+        return this.tabCompte.length;
+    }
+    public Compte getCompte(int numeroDeCompte){
+        return tabCompte[numeroDeCompte]
+    }
+    // Setters
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    // Methodes
+    public void ajouterCompte(){
+        this.tabCompte[this.nombreDeComptes] = new Compte();
+    }
+
+    public void fermerCompte(){
+        
+    }
+
 }
